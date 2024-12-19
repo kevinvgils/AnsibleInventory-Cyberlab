a5cb4a7a43939502e4ecb3cfb7192e8c16ae5874
Kevin van Gils <kvnvangils@gmail.com>
2024-12-19 11:48:36 +0100

    test terraform

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,5 +1,5 @@
 resource "proxmox_vm_qemu" "example" {
   name        = "example-vm"  # Naam van je VM
   target_node = "proxmox"   # De node waarop de VM moet draaien
-  clone       = "UbuntuOS"  # De naam van het template
+  clone       = "test"  # De naam van het template
 }
