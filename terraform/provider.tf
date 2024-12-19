4524e15f76efe0d1c750c57f5fa7d863136aadf0
Kevin van Gils <kvnvangils@gmail.com>
2024-12-19 11:04:52 +0100

    test terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,7 +14,7 @@ provider "proxmox"{
   pm_api_token_id     = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure     = true # Set to false if using a valid SSL certificate
-  pm_user             = "testing" # Ensure the username is correct
+  pm_user             = "testing@pve" # Ensure the username is correct
 }
 
 
