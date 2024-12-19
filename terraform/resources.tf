91241501ce99eb3938860f16e7fabfc92a82a78a
Kevin van Gils <kvnvangils@gmail.com>
2024-12-19 11:54:12 +0100

    test terraform

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -2,4 +2,10 @@ resource "proxmox_vm_qemu" "example" {
   name        = "example-vm"  # Naam van je VM
   target_node = "proxmox"   # De node waarop de VM moet draaien
   clone       = "test"  # De naam van het template
+
+  timeout {
+    create = "60m"  # 60 minutes
+    update = "60m"
+    delete = "30m"
+  }
 }
